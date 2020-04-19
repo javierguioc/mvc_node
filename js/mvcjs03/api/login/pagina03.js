51c3a7a457d3061e0d89e51afae6d89f6d22c78e
javier <hernan0114@gmail.com>
2020-04-18 22:50:50 -0500

    Redireccionamiento de pag 2 a pag 3

--- js/mvcjs03/api/login/pagina03.js
+++ js/mvcjs03/api/login/pagina03.js
@@ -1,19 +1,17 @@
-module.exports = async function (res,datos) {
-  console.log("object APgina03: ", datos);
-res.writeHead(200, { "Content-Type": "text/html" });
-res.write(`
+module.exports = async function (res, datos) {
+  // console.log("object APgina03: ", datos);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`
 <h3>Usuario: ${datos["usu_login"]} </h3>
 <h3>Rol:  ${datos["result"]["rol_nombre"]} </h3>
 <h3>Descripcion:  ${datos["result"]["rol_descripcion"]} </h3>
 <h3>Funcionalidades:  </h3>
 <ul>`);
-datos["FunxUsu"].forEach((element) => {
-  res.write(`<li> ${element.fun_nombre}</li>`);
-});
-res.write(`</ul>`);
+  datos["FunxUsu"].forEach((element) => {
+    res.write(`<li> ${element.fun_nombre}</li>`);
+  });
+  res.write(`</ul>`);
 
-res.write(`<br><button onclick="window.location.href = '/'">Salir</button> `);
-res.end();
-
-}
-
+  res.write(`<br><button onclick="window.location.href = '/'">Salir</button> `);
+  res.end();
+};
